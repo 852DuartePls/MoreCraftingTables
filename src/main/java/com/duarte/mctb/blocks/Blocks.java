ebf1f30127bcdbbc35875023fc6a1920c4ba5ea2
852DuartePls <73360977+852DuartePls@users.noreply.github.com>
2022-05-24 14:07:35 -0400

    Update Blocks.java

--- src/main/java/com/duarte/mctb/blocks/Blocks.java
+++ src/main/java/com/duarte/mctb/blocks/Blocks.java
@@ -1,48 +1,45 @@
 package com.duarte.mctb.blocks;
 
-import com.duarte.mctb.MoreCraftingTables;
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.util.ResourceLocation;
-
+import com.duarte.mctb.blocks.CraftingBlock;
 import java.util.ArrayList;
 import java.util.List;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 public class Blocks {
-    public static List<Block> blockList = new ArrayList<>();
-
-
- // Vanilla //
-    public static final Block SPRUCE_CRAFTING_TABLE = craftingTable("spruce_crafting_table");
-    public static final Block BIRCH_CRAFTING_TABLE = craftingTable("birch_crafting_table");
-    public static final Block ACACIA_CRAFTING_TABLE = craftingTable("acacia_crafting_table");
-    public static final Block JUNGLE_CRAFTING_TABLE = craftingTable("jungle_crafting_table");
-    public static final Block DARK_OAK_CRAFTING_TABLE = craftingTable("dark_oak_crafting_table");
-    public static final Block WARPED_CRAFTING_TABLE = craftingTable("warped_crafting_table");
-    public static final Block CRIMSON_CRAFTING_TABLE = craftingTable("crimson_crafting_table");
-
-    // Biomes 'o Plenty
-    public static final Block CHERRY_CRAFTING_TABLE = craftingTable("cherry_crafting_table");
-    public static final Block DEAD_CRAFTING_TABLE = craftingTable("dead_crafting_table");
-    public static final Block FIR_CRAFTING_TABLE = craftingTable("fir_crafting_table");
-    public static final Block HELLBARK_CRAFTING_TABLE = craftingTable("hellbark_crafting_table");
-    public static final Block JACARANDA_CRAFTING_TABLE = craftingTable("jacaranda_crafting_table");
-    public static final Block MAGIC_CRAFTING_TABLE = craftingTable("magic_crafting_table");
-    public static final Block MAHOGANY_CRAFTING_TABLE = craftingTable("mahogany_crafting_table");
-    public static final Block PALM_CRAFTING_TABLE = craftingTable("palm_crafting_table");
-    public static final Block REDWOOD_CRAFTING_TABLE = craftingTable("redwood_crafting_table");
-    public static final Block UMBRAN_CRAFTING_TABLE = craftingTable("umbran_crafting_table");
-    public static final Block WILLOW_CRAFTING_TABLE = craftingTable("willow_crafting_table");
-
+    public static List<Block> blockList = new ArrayList<Block>();
+    //Vanilla
+    public static final Block SPRUCE_CRAFTING_TABLE = Blocks.craftingTable("spruce_crafting_table");
+    public static final Block BIRCH_CRAFTING_TABLE = Blocks.craftingTable("birch_crafting_table");
+    public static final Block ACACIA_CRAFTING_TABLE = Blocks.craftingTable("acacia_crafting_table");
+    public static final Block JUNGLE_CRAFTING_TABLE = Blocks.craftingTable("jungle_crafting_table");
+    public static final Block DARK_OAK_CRAFTING_TABLE = Blocks.craftingTable("dark_oak_crafting_table");
+    public static final Block WARPED_CRAFTING_TABLE = Blocks.craftingTable("warped_crafting_table");
+    public static final Block CRIMSON_CRAFTING_TABLE = Blocks.craftingTable("crimson_crafting_table");
+    // Biomes O' Plenty
+    public static final Block CHERRY_CRAFTING_TABLE = Blocks.craftingTable("cherry_crafting_table");
+    public static final Block DEAD_CRAFTING_TABLE = Blocks.craftingTable("dead_crafting_table");
+    public static final Block FIR_CRAFTING_TABLE = Blocks.craftingTable("fir_crafting_table");
+    public static final Block HELLBARK_CRAFTING_TABLE = Blocks.craftingTable("hellbark_crafting_table");
+    public static final Block JACARANDA_CRAFTING_TABLE = Blocks.craftingTable("jacaranda_crafting_table");
+    public static final Block MAGIC_CRAFTING_TABLE = Blocks.craftingTable("magic_crafting_table");
+    public static final Block MAHOGANY_CRAFTING_TABLE = Blocks.craftingTable("mahogany_crafting_table");
+    public static final Block PALM_CRAFTING_TABLE = Blocks.craftingTable("palm_crafting_table");
+    public static final Block REDWOOD_CRAFTING_TABLE = Blocks.craftingTable("redwood_crafting_table");
+    public static final Block UMBRAN_CRAFTING_TABLE = Blocks.craftingTable("umbran_crafting_table");
+    public static final Block WILLOW_CRAFTING_TABLE = Blocks.craftingTable("willow_crafting_table");
+    // Quark
+    public static final Block AZALEA_CRAFTING_TABLE = Blocks.craftingTable("azalea_crafting_table");
+    public static final Block BLOSSOM_CRAFTING_TABLE = Blocks.craftingTable("blossom_crafting_table");
 
     static Block craftingTable(String id) {
-        Block createBlock = new CraftingBlock(AbstractBlock.Properties.from(net.minecraft.block.Blocks.CRAFTING_TABLE));
-        createBlock.setRegistryName(new ResourceLocation(MoreCraftingTables.MOD_ID, id));
-        blockList.add(createBlock);
+        CraftingBlock createBlock = new CraftingBlock(BlockBehaviour.Properties.copy(net.minecraft.world.level.block.Blocks.CRAFTING_TABLE));
+        createBlock.setRegistryName(new ResourceLocation("mctb", id));
+        blockList.add((Block)createBlock);
         return createBlock;
     }
 
     public static void init() {
     }
-
 }
