58914d3cd08fbf5084c224e449082240b81b1796
852DuartePls <73360977+852DuartePls@users.noreply.github.com>
2023-09-09 15:28:17 -0400

    Update MoreCraftingTables.java
    added event for the crafting tables to be used as fuel

--- src/main/java/com/duart/mctb/MoreCraftingTables.java
+++ src/main/java/com/duart/mctb/MoreCraftingTables.java
@@ -3,14 +3,15 @@ package com.duart.mctb;
 import com.duart.mctb.blocks.CraftingBlock;
 import com.duart.mctb.blocks.ModBlocks;
 import com.duart.mctb.blocks.Registration;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.furnace.FurnaceFuelBurnTimeEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.RegistryObject;
 import org.apache.logging.log4j.LogManager;
@@ -21,22 +22,15 @@ public class MoreCraftingTables {
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "mctb";
 
-    public static boolean isClient = false;
 
     public MoreCraftingTables() {
         ModBlocks.registerBlockItems();
         Registration.register();
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
         MinecraftForge.EVENT_BUS.register(this);
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener(this::addToExistingTab);
-    }
 
-    private void commonSetup(FMLCommonSetupEvent event) {}
-
-    private void doClientStuff(FMLClientSetupEvent event) {
-        isClient = true;
+        LOGGER.info("MoreCraftingTables Loaded");
     }
 
     @SubscribeEvent
@@ -46,5 +40,31 @@ public class MoreCraftingTables {
                 event.accept(blockObject.get());
             }
         }
+    }
+
+    // Allow Crafting Tables to be used as furnace fuel
+    @SubscribeEvent
+    public void onFurnaceFuelBurnTime(FurnaceFuelBurnTimeEvent event) {
+        Item item = event.getItemStack().getItem();
+
+        if (isCustomCraftingTableItem(item)) {
+            event.setBurnTime(300);
+        }
+    }
+    private boolean isCustomCraftingTableItem(Item item) {
+        for (RegistryObject<CraftingBlock> blockObject : ModBlocks.CRAFTING_TABLES) {
+            ResourceLocation blockLocation = blockObject.getId();
+            
+    //Except from the Warped and Crimson varaints
+            if ("mctb:warped_crafting_table".equals(blockLocation.toString()) ||
+                    "mctb:crimson_crafting_table".equals(blockLocation.toString())) {
+                continue;
+            }
+
+            if (item == Item.BY_BLOCK.get(blockObject.get())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
